42bd24574ccbd4b057ee8765dde44de81a5ee9a0
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-05 16:04:54 +0900

    400 에러 테스트

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -16,7 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT토큰");
 
@@ -29,5 +31,10 @@ public class MemberController {
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid(){
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() ==0;
+        }
     }
+
 }
